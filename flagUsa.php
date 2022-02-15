007fa691f4b4e4a55116ddc5fb5f363461672a20
alireza Gholamrezaie <45558196+alirezaghd@users.noreply.github.com>
2022-02-15 21:48:40 +0330

    Add files via upload

---
+++ flagUsa.php
@@ -0,0 +1,33 @@
+<html>
+
+<head>
+	<meta name="description" content="design USA Flag">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="keywords" content="flag,star,USA">
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+	<div class="stars">
+		<?php
+		$stars = '<img  src="star.png" alt="Image" width="13" height="13" />'; ?>
+		<?php for ($i = 0; $i < 55; $i++) : ?>
+			<span>
+				<?php echo ($stars); ?>
+			<?php endfor; ?>
+			</span>
+	</div>
+	<div>
+		<table border="1" class="flag">
+			<?php for ($i = 0; $i < 7; $i++) : ?>
+				<tr>
+					<td class="V_flag"></td>
+				</tr>
+			<?php endfor; ?>
+		</table>
+	</div>
+</body>
+
+</html>
